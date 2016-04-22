f3eeefe977f45071fa314b2b352660a392c0a972
Vaclav Mlejnsky <vasek.mlejnsky@gmail.com>
2016-04-22 17:26:13 +0200

    Few changes before integrating backend

--- upload.php
+++ upload.php
@@ -2,40 +2,40 @@
 /* creates a compressed zip file */
 if (!function_exists('parseDate')) {
 	    function create_zip($files = array(), $file_names = array(), $destination = '', $overwrite = false) {
-		//if the zip file already exists and overwrite is false, return false
+		// if the zip file already exists and overwrite is false, return false
 		if (file_exists($destination) && !$overwrite)
 			return false;
-		//vars
+		// vars
 		$valid_files = array();
-		//if files were passed in...
+		// if files were passed in...
 		if (is_array($files)) {
-			//cycle through each file
+			// cycle through each file
 			foreach ($files as $file) {
-				//make sure the file exists
+				// make sure the file exists
 				if (file_exists($file)) {
 					$valid_files[] = $file;
 				}
 			}
 		}
-		//if we have good files...
+		// if we have good files...
 		if(count($valid_files)) {
-			//create the archive
+			// create the archive
 			$zip = new ZipArchive();
 			if ($zip->open($destination,$overwrite ? ZIPARCHIVE::OVERWRITE : ZIPARCHIVE::CREATE) !== true) {
 				return false;
 			}
-			//add the files
+			// add the files
 			for ($i = 0; $i < count($valid_files); $i++) {
 				$zip->addFile($valid_files[$i], $file_names[$i]);
 			}
 
-			//debug
+			// debug
 			//echo 'The zip archive contains ',$zip->numFiles,' files with a status of ',$zip->status;
-			
-			//close the zip -- done!
+
+			// close the zip -- done!
 			$zip->close();
-			
-			//check to make sure the file exists
+
+			// check to make sure the file exists
 			return file_exists($destination);
 		}
 		else
@@ -61,9 +61,9 @@ $new_name = '';
 
 
 //if (!empty($_FILES)) {
-	
+
     try {
-    
+
     	// initialization of '$name_array'
     	foreach ($_FILES['file']['tmp_name'] as $index => $tmpName) {
     		$name = $_FILES['file']['name'][$index];
@@ -82,13 +82,13 @@ $new_name = '';
 				$name_array[$name]++;
 			}
 
-			$_FILES['file']['name'][$index] = $new_name;			
+			$_FILES['file']['name'][$index] = $new_name;
 		}
 
     	$randomFoldername = explode(".", uniqid(rand(), true), 2)[0];
     	$zipName = $randomFoldername . ".zip";
     	$zipPath = $_SERVER['DOCUMENT_ROOT'] . '/' . $zipName;
-		create_zip($_FILES['file']['tmp_name'], $_FILES['file']['name'], $zipPath);	
+		create_zip($_FILES['file']['tmp_name'], $_FILES['file']['name'], $zipPath);
 
 		chmod($_SERVER['DOCUMENT_ROOT'], 0777);
 		chmod($zipName, 0777);
@@ -98,7 +98,7 @@ $new_name = '';
 		file_put_contents("php://stderr", "---------" . "File size: " . filesize($zipName) . "\n");
 		file_put_contents("php://stderr", "---------" . "File exists: " . file_exists($zipName) . "\n");
 		file_put_contents("php://stderr", "---------" . "File readable: " . is_readable($zipName) . "\n");
-		file_put_contents("php://stderr", "---------" . "File writable: " . is_writable($zipName) . "\n");	
+		file_put_contents("php://stderr", "---------" . "File writable: " . is_writable($zipName) . "\n");
 		$fp = fopen($zipName, 'a+');
 		if (!$fp) {
 			file_put_contents("php://stderr", "---------" . "Cannot open file.\n");
@@ -108,14 +108,14 @@ $new_name = '';
 		}
 
 
-		// PROC SE VYTVARI DALSI .ZIP FILE? Protoze pri deploy se spusti vsechny soubory a uploadne se jeden 0B file	
-		
-	
-		$upload = $s3->upload($bucket, $zipName, fopen($zipName, 'r'), 'public-read'); 
+		// PROC SE VYTVARI DALSI .ZIP FILE? Protoze pri deploy se spusti vsechny soubory a uploadne se jeden 0B file
+
+
+		$upload = $s3->upload($bucket, $zipName, fopen($zipName, 'r'), 'public-read');
 		$GLOBALS['linkURL'] = $upload->get('ObjectURL');
 		echo $GLOBALS['linkURL'];
-		
-		/*				
+
+		/*
 		$upload = $s3->putObject(array(
     		'Bucket' => $bucket,
 		    'Key' => $zipName,
@@ -125,13 +125,13 @@ $new_name = '';
 		    'ContentType' => 'application/zip'
 		));
 		*/
-		
+
 		//$linkURL = $s3->getObjectUrl($bucket, $zipName);
 		//$GLOBALS['linkURL'] = $linkURL;
 		//$GLOBALS['linkURL'] = $upload->get('ObjectURL');
 		//array_push($link_arr, $upload->get('ObjectURL'));
 
-		
+
 
 /*
     	// Upload kazdeho filu
@@ -153,11 +153,11 @@ $new_name = '';
 	    }
 */
     }
-    
+
     catch (Exception $e) {
     	$GLOBALS['linkURL'] = 'Exception: ' . $e->getMessage();
     }
 //}
-    
+
 //}
-?>   
+?>
